6816a86c76f7e9a92c18a8e2482e544f6c0048aa
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-05-25 19:04:34 -0400

    Fix track extension (#40)

--- tools/oilOutpost.js
+++ tools/oilOutpost.js
@@ -313,7 +313,7 @@ module.exports = function(string, opt = {}) {
   if (INCLUDE_TRAIN_STATION) {
     trainStopLocation = generateTrainStation(bp,
        { x: target.x + 3 + 3 * TANKS, y: target.y - 2 },
-        Math.max(bp.bottomRight().y, target.y + 2 + WAGONS * 7 + ((SINGLE_HEADED_TRAIN ? 0 : 1) * LOCOMOTIVES)), 
+        Math.max(bp.bottomRight().y, target.y + 2 + WAGONS * 7 + ((SINGLE_HEADED_TRAIN ? 0 : 1) * LOCOMOTIVES) * 7), 
         {
       LOCOMOTIVES,
       TRACK_CONCRETE,
