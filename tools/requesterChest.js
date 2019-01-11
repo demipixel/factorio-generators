80590b5a2090389158d607ae9e4875e92c691e22
Lucas Simon <luke5227@gmail.com>
2019-01-11 15:55:07 -0800

    Make blueprint chests in a square, center blueprint.

--- tools/requesterChest.js
+++ tools/requesterChest.js
@@ -1,9 +1,12 @@
 const Blueprint = require('factorio-blueprint');
-const RequestChestSlotCount = 12;
-const RailsPerCurvedRail = 4;
-const RailsPerStraightRail = 1;
+const REQUEST_CHEST_SLOT_COUNT = 12;
+const RAILS_PER_CURVED_RAIL = 4;
+const RAILED_PER_STRAIGHT_RAIL = 1;
 
 module.exports = function(string) {
+    if (Blueprint.isBook(string)) {
+        throw new Error('Blueprint string must be a blueprint, not a book.');
+    }
     const importedBP = new Blueprint(string);
     const requestBP = new Blueprint();
 
@@ -30,29 +33,33 @@ module.exports = function(string) {
     // replace rails
     let railCount = 0;
     if (entityCounts.has('curved_rail')) {
-        railCount += entityCounts.get('curved_rail') * RailsPerCurvedRail;
+        railCount += entityCounts.get('curved_rail') * RAILS_PER_CURVED_RAIL;
         entityCounts.delete('curved_rail');
     }
     if (entityCounts.has('straight_rail')) {
-        railCount += entityCounts.get('straight_rail') * RailsPerStraightRail;
+        railCount += entityCounts.get('straight_rail') * RAILED_PER_STRAIGHT_RAIL;
         entityCounts.delete('straight_rail');
     }
     if (railCount > 0) {
         entityCounts.set('rail', railCount);
     }
 
-    const chestCount = Math.ceil(entityCounts.size / RequestChestSlotCount);
+    const chestCount = Math.ceil(entityCounts.size / REQUEST_CHEST_SLOT_COUNT);
+    const squareSize = Math.ceil(Math.sqrt(chestCount));
 
     let requestItemId = 0;
     let entities = entityCounts.entries();
     let entry = entities.next();
     for (let i = 0; i < chestCount; i++) {
-        let requestEntity = requestBP.createEntity('logistic_chest_requester', {x: i, y: 0});
-        for (let j = 0; j < RequestChestSlotCount && !entry.done; j++) {
+        const x = i % squareSize;
+        const y = Math.floor(i/squareSize);
+        let requestEntity = requestBP.createEntity('logistic_chest_requester', {x, y});
+        for (let j = 0; j < REQUEST_CHEST_SLOT_COUNT && !entry.done; j++) {
             requestEntity.setRequestFilter(j, entry.value[0], entry.value[1]);
             entry = entities.next();
         }
     }
 
+    requestBP.center();
     return requestBP.encode();
 };
