4795bd6239ed42787036de2ff0933af5c0e199da
WerFra <WerFra@users.noreply.github.com>
2019-01-11 23:54:03 +0100

    Generate blueprint with requester chests that contain all the items required by the blueprint provided

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 module.exports = {
   outpost: require('./tools/outpost'),
   blueprintTool: require('./tools/blueprintTool'),
-  oilOutpost: require('./tools/oilOutpost')
+  oilOutpost: require('./tools/oilOutpost'),
+  requesterChest: require('./tools/requesterChest')
 };
